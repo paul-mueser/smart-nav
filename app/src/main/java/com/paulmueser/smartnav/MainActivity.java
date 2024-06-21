9d62be63ec01dbaf5c407c373f0b94296666bad7
Paul Müser <kontakt@paulmueser.de>
2024-06-21 14:50:44 +0200

    add first fragments and move some stuff around

--- app/src/main/java/com/paulmueser/smartnav/MainActivity.java
+++ app/src/main/java/com/paulmueser/smartnav/MainActivity.java
@@ -3,19 +3,27 @@ package com.paulmueser.smartnav;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import com.paulmueser.smartnav.api.ApiService;
 import com.paulmueser.smartnav.api.IResponseReceived;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.paulmueser.smartnav.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ApiService apiService = new ApiService();
 
-        setContentView(R.layout.activity_main);
-        Log.i("SmartNav", "Started");
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+    }
+
+    // TODO this is the way to call the request method
+    private void request() {
         ApiService.sendRequest(new IResponseReceived() {
             @Override
             public void onSuccess(String response) {
